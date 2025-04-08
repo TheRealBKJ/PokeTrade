dad09cf5fc7cce06ecbf0def2622ff5421f67dda
Dobromir <127112391+dobromiriiliev@users.noreply.github.com>
2025-04-08 15:38:17 -0400

    added urls.py to each of the directories

--- PokeTrade/PokeTrade/urls.py
+++ PokeTrade/PokeTrade/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Include app URLs
+    path('api/', include('api.urls')),
+    path('marketplace/', include('marketplace.urls')),
+    path('notifications/', include('notifications.urls')),
+    path('trades/', include('trades.urls')),
+    path('users/', include('users.urls')),
+
 ]
---
+++ PokeTrade/api/urls.py
@@ -0,0 +1,6 @@
+# api/urls.py (temporary test)
+from django.urls import path
+from . import views
+
+urlpatterns = [
+]
