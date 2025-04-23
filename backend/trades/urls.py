3ebbcbe190298097a31770943631c310444049df
Dobromir <127112391+dobromiriiliev@users.noreply.github.com>
2025-04-23 17:31:18 -0400

    fixed urls.py in trades by adding the proper import statement

--- backend/trades/urls.py
+++ backend/trades/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import TradeListCreateView
+from .views import accept_trade
+from .views import reject_trade
 
 urlpatterns = [
     path('', TradeListCreateView.as_view(), name='trade-list-create'),
