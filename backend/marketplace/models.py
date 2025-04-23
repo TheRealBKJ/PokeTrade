63ece26997113565a2b12941fe78620cc3c411e0
Braden Jones <98116111+TheRealBKJ@users.noreply.github.com>
2025-04-23 15:59:30 -0400

    added a model for. alisting on marketplace

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Listing(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
+    card_id = models.CharField(max_length=100)  # TCG API card id
+    card_name = models.CharField(max_length=255)
+    card_image_url = models.URLField()
+
+    listing_type = models.CharField(max_length=10, choices=[
+        ('trade', 'Trade'),
+        ('sale', 'Sale')
+    ], default='trade')
+
+    price = models.IntegerField(null=True, blank=True)  # Only if it's for sale
+
+    is_active = models.BooleanField(default=True)  # Listing still active or not
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.card_name} by {self.owner.username}"
