3f4e07bc25cc720933f9a25bcc86b6ca4a2f9780
Braden Jones <98116111+TheRealBKJ@users.noreply.github.com>
2025-04-28 20:53:45 -0400

    fixing axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,24 +1,9 @@
 // frontend/src/axios.js
 import axios from 'axios';
 
-<<<<<<< Updated upstream
-const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
-  headers: { 'Content-Type': 'application/json' }
-});
-
-// On app load, read any saved token and set the Authorization header
-const token = localStorage.getItem('access_token');
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
-
-export default api;
-=======
 const instance = axios.create({
   baseURL: '/api/',   // 👈 This is the critical part
 });
 
 export default instance;
 
->>>>>>> Stashed changes
